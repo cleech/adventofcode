dacbcb8c7fa36fdef0e2231572d24542417d143f
Chris Leech <leech@pobox.com>
2015-12-02 23:12:21 -0800

    day 3
    a bit ugly still, trying to do as much as possible with iterators

--- src/day3.rs
+++ src/day3.rs
@@ -1,19 +1,85 @@
+extern crate itertools;
+use self::itertools::Itertools;
+
 pub fn main(input: &str) -> Vec<String> {
-    let s1 = three_one(&input);
-    let s2 = three_two(&input);
+    let s1 = house_count(&input);
+    let s2 = robo_santa(&input);
     vec![s1.to_string(), s2.to_string()]
 }
 
-fn three_one(input: &str) -> i32 { -1 }
-
-#[test]
-fn test_three_one() {
-    assert_eq!(three_one(""), -1);
+fn directions(c: char) -> (i32, i32) {
+    match c {
+        '>' => (1, 0),
+        '<' => (-1, 0),
+        '^' => (0, 1),
+        'v' => (0, -1),
+        _ => panic!("invalid input"),
+    }
 }
 
-fn three_two(input: &str) -> i32 { -1 }
+fn house_count(input: &str) -> usize {
+    let mut vec = input.chars()
+                       .map(directions)
+                       .scan((0, 0), |&mut (ref mut x, ref mut y), (dx, dy)| {
+                           *x += dx;
+                           *y += dy;
+                           Some((*x, *y))
+                       })
+                       .collect::<Vec<(i32, i32)>>();
+    // .inspect(|&(x, y)| println!("{}, {}", x, y))
+    vec.push((0, 0));
+    vec.sort();
+    vec.dedup();
+    vec.len()
+}
 
 #[test]
-fn test_three_two() {
-    assert_eq!(three_two(""), -1);
+fn test_house_count() {
+    assert_eq!(house_count(">"), 2);
+    assert_eq!(house_count("^>v<"), 4);
+    assert_eq!(house_count("^v^v^v^v^v"), 2);
+}
+
+fn robo_santa(input: &str) -> usize {
+    let map = input.chars()
+                   .map(directions)
+                   .scan((0 as usize, 0 as i32, 0 as i32),
+                         |&mut (ref mut index, _, _), (dx, dy)| {
+                             *index += 1;
+                             Some((*index, dx, dy))
+                         });
+
+    let (even, odd): (Vec<(usize, i32, i32)>, Vec<(usize, i32, i32)>) =
+        map.partition(|&(index, _, _)| index % 2 == 0);
+
+    let mut vec = even.iter()
+                      .map(|&(_, x, y)| (x, y))
+                      .scan((0, 0), |&mut (ref mut x, ref mut y), (dx, dy)| {
+                          *x += dx;
+                          *y += dy;
+                          Some((*x, *y))
+                      })
+                      .collect::<Vec<(i32, i32)>>();
+
+    let vec2 = odd.iter()
+                  .map(|&(_, x, y)| (x, y))
+                  .scan((0, 0), |&mut (ref mut x, ref mut y), (dx, dy)| {
+                      *x += dx;
+                      *y += dy;
+                      Some((*x, *y))
+                  })
+                  .collect::<Vec<(i32, i32)>>();
+
+    vec.push_all(&vec2);
+    vec.push((0, 0));
+    vec.sort();
+    vec.dedup();
+    vec.len()
+}
+
+#[test]
+fn test_robo_santa() {
+    assert_eq!(robo_santa("^v"), 3);
+    assert_eq!(robo_santa("^>v<"), 3);
+    assert_eq!(robo_santa("^v^v^v^v^v"), 11);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(iter_arith)] // sum is not stable
+#![feature(vec_push_all)]
 
 use std::io;
 use std::fs::File;
@@ -22,8 +23,8 @@ fn try_main() -> io::Result<()> {
     let s = try!(run_from_file("input_2.txt", day2::main));
     s.iter().map(|s| println!("{}", s)).collect::<Vec<()>>();
 
-//    let s = try!(run_from_file("input_3.txt", day3::main));
-//    s.iter().map(|s| println!("{}", s)).collect::<Vec<()>>();
+    let s = try!(run_from_file("input_3.txt", day3::main));
+    s.iter().map(|s| println!("{}", s)).collect::<Vec<()>>();
 
     Ok(())
 }
@@ -39,8 +40,10 @@ fn run_from_file<F>(file: &str, it: F) -> io::Result<Vec<String>>
 
 #[test]
 fn verify_my_answers() {
-    assert_eq!(run_from_file("input_1.txt", day1::main).unwrap(), vec!["74", "1795"]);
-    assert_eq!(run_from_file("input_2.txt", day2::main).unwrap(), vec!["1586300", "3737498"]);
-//    assert_eq!(run_from_file("input_3.txt", day3::main).unwrap(), vec!["-1", "-1"]);
+    assert_eq!(run_from_file("input_1.txt", day1::main).unwrap(),
+               vec!["74", "1795"]);
+    assert_eq!(run_from_file("input_2.txt", day2::main).unwrap(),
+               vec!["1586300", "3737498"]);
+    assert_eq!(run_from_file("input_3.txt", day3::main).unwrap(),
+               vec!["2565", "2639"]);
 }
-
