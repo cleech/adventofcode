8e54a0078123aee34b882cb810cca695ddfe6756
Chris Leech <leech@pobox.com>
2015-12-04 14:48:48 -0800

    re-inline code now that parts 1 and 2 use the same function

--- src/day3.rs
+++ src/day3.rs
@@ -9,33 +9,31 @@ pub fn main(input: &str) -> Vec<String> {
     vec![s1.to_string(), s2.to_string()]
 }
 
-fn directions(c: char) -> (i32, i32) {
-    match c {
-        '>' => (1, 0),
-        '<' => (-1, 0),
-        '^' => (0, 1),
-        'v' => (0, -1),
-        _ => panic!("invalid input"),
-    }
-}
-
-fn visit_houses<'a>(input: Box<Iterator<Item = char> + 'a>) -> Box<Iterator<Item = (i32, i32)> + 'a> {
-    Box::new(input.map(directions)
-                  .scan((0, 0), |&mut (ref mut x, ref mut y), (dx, dy)| {
-                      *x += dx;
-                      *y += dy;
-                      Some((*x, *y))
-                  }))
-}
-
 fn multi_santa(input: &str, how_many: usize) -> usize {
-    return (0..how_many)
-               .map(|n| Box::new(input.chars().skip(n).step(how_many)))
-               .map(|directions| visit_houses(directions))
-               .fold(Box::new(iter::once((0, 0))) as Box<Iterator<Item = (i32, i32)>>,
-                     |itr, next_santa| Box::new(itr.chain(next_santa)))
-               .unique()
-               .count();
+    (0..how_many)
+        .map(|n| {
+            input.chars()
+                 .skip(n)
+                 .step(how_many)
+                 .map(|c| {
+                     match c {
+                         '>' => (1, 0),
+                         '<' => (-1, 0),
+                         '^' => (0, 1),
+                         'v' => (0, -1),
+                         _ => panic!("invalid input"),
+                     }
+                 })
+                 .scan((0, 0), |&mut (ref mut x, ref mut y), (dx, dy)| {
+                     *x += dx;
+                     *y += dy;
+                     Some((*x, *y))
+                 })
+        })
+        .fold(Box::new(iter::once((0, 0))) as Box<Iterator<Item = (i32, i32)>>,
+              |itr, next_santa| Box::new(itr.chain(next_santa)))
+        .unique()
+        .count()
 }
 
 #[test]
