17290ce6e62bb170333ea32681eabe367e0933d1
Chris Leech <leech@pobox.com>
2015-12-04 13:37:59 -0800

    multi santa

--- src/day3.rs
+++ src/day3.rs
@@ -4,8 +4,8 @@ extern crate itertools;
 use self::itertools::Itertools;
 
 pub fn main(input: &str) -> Vec<String> {
-    let s1 = house_count(&input);
-    let s2 = robo_santa(&input);
+    let s1 = multi_santa(&input, 1);
+    let s2 = multi_santa(&input, 2);
     vec![s1.to_string(), s2.to_string()]
 }
 
@@ -26,36 +26,31 @@ fn visit_houses<'a>(input: &'a str) -> Box<Iterator<Item = (i32, i32)> + 'a> {
                       *x += dx;
                       *y += dy;
                       Some((*x, *y))
-                  })
-                  .chain(iter::once((0, 0))))
+                  }))
+    // .chain(iter::once((0, 0))))
 }
 
-fn house_count(input: &str) -> usize {
-    visit_houses(input)
-        .unique()
-        .count()
+fn multi_santa(input: &str, how_many: usize) -> usize {
+    let santas = (0..how_many)
+                     .map(|n| input.chars().skip(n).step(how_many).collect())
+                     .collect::<Vec<String>>();
+    return santas.iter()
+                 .fold(Box::new(iter::once((0, 0))) as Box<Iterator<Item = (i32, i32)>>,
+                       |itr, directions| Box::new(itr.chain(visit_houses(&directions))))
+                 .unique()
+                 .count();
 }
 
 #[test]
 fn test_house_count() {
-    assert_eq!(house_count(">"), 2);
-    assert_eq!(house_count("^>v<"), 4);
-    assert_eq!(house_count("^v^v^v^v^v"), 2);
-}
-
-fn robo_santa(input: &str) -> usize {
-    let odd: String = input.chars().step(2).collect();
-    let even: String = input.chars().skip(1).step(2).collect();
-
-    return visit_houses(&odd)
-               .chain(visit_houses(&even))
-               .unique()
-               .count();
+    assert_eq!(multi_santa(">", 1), 2);
+    assert_eq!(multi_santa("^>v<", 1), 4);
+    assert_eq!(multi_santa("^v^v^v^v^v", 1), 2);
 }
 
 #[test]
 fn test_robo_santa() {
-    assert_eq!(robo_santa("^v"), 3);
-    assert_eq!(robo_santa("^>v<"), 3);
-    assert_eq!(robo_santa("^v^v^v^v^v"), 11);
+    assert_eq!(multi_santa("^v", 2), 3);
+    assert_eq!(multi_santa("^>v<", 2), 3);
+    assert_eq!(multi_santa("^v^v^v^v^v", 2), 11);
 }
