d3074206880cabde1b3672c95214c7812601444f
Chris Leech <leech@pobox.com>
2015-12-03 21:50:28 -0800

    day 4

---
+++ src/day4.rs
@@ -0,0 +1,31 @@
+extern crate crypto;
+use self::crypto::digest::Digest;
+use self::crypto::md5::Md5;
+
+pub fn main(input: &str) -> Vec<String> {
+    let s1 = find_number(input, 5);
+    let s2 = find_number(input, 6);
+    vec![s1.to_string(), s2.to_string()]
+}
+
+fn find_number(key: &str, zeros: usize) -> u32 {
+    let mut sh = Md5::new();
+    for n in 1.. {
+        let input = format!("{}{}", key, n);
+
+        sh.input_str(&input);
+        let output = sh.result_str();
+        sh.reset();
+
+        if &output[..zeros] == &"0000000000"[..zeros] {
+            return n;
+        }
+    }
+    0
+}
+
+#[test]
+fn test_find_number() {
+    assert_eq!(find_number("abcdef", 5), 609043);
+    assert_eq!(find_number("pqrstuv", 5), 1048970);
+}
--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,23 @@ extern crate scan_fmt;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     try_main().unwrap()
 }
 
 fn try_main() -> io::Result<()> {
-    let s = try!(run_from_file("input_1.txt", day1::main));
+    let s = try!(run_from_file("data/input_1.txt", day1::main));
     s.iter().map(|s| println!("{}", s)).collect::<Vec<()>>();
 
-    let s = try!(run_from_file("input_2.txt", day2::main));
+    let s = try!(run_from_file("data/input_2.txt", day2::main));
     s.iter().map(|s| println!("{}", s)).collect::<Vec<()>>();
 
-    let s = try!(run_from_file("input_3.txt", day3::main));
+    let s = try!(run_from_file("data/input_3.txt", day3::main));
+    s.iter().map(|s| println!("{}", s)).collect::<Vec<()>>();
+
+    let s = day4::main("yzbqklnj");
     s.iter().map(|s| println!("{}", s)).collect::<Vec<()>>();
 
     Ok(())
@@ -39,10 +43,11 @@ fn run_from_file<F>(file: &str, it: F) -> io::Result<Vec<String>>
 
 #[test]
 fn verify_my_answers() {
-    assert_eq!(run_from_file("input_1.txt", day1::main).unwrap(),
+    assert_eq!(run_from_file("data/input_1.txt", day1::main).unwrap(),
                vec!["74", "1795"]);
-    assert_eq!(run_from_file("input_2.txt", day2::main).unwrap(),
+    assert_eq!(run_from_file("data/input_2.txt", day2::main).unwrap(),
                vec!["1586300", "3737498"]);
-    assert_eq!(run_from_file("input_3.txt", day3::main).unwrap(),
+    assert_eq!(run_from_file("data/input_3.txt", day3::main).unwrap(),
                vec!["2565", "2639"]);
+    assert_eq!(day4::main("yzbqklnj"), vec!["282749", "9962624"]);
 }
