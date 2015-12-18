835cfba85216e3ed69ec82db8a6e5a573b53dc92
Chris Leech <leech@pobox.com>
2015-12-18 09:47:29 -0800

    day 17: just track slices of remaining input data instead of copying out to vec

--- src/day17.rs
+++ src/day17.rs
@@ -21,32 +21,33 @@ fn part2() -> usize {
     v.iter().filter(|ss| ss.len() == min).count()
 }
 
-struct PowerSet<T, F>
-    where T: Copy,
+struct PowerSet<'a, T, F>
+    where T: 'a + Copy,
           F: Fn(&[T]) -> bool
 {
-    stack: Vec<(Vec<T>, Vec<T>)>,
+    stack: Vec<(Vec<T>, &'a [T])>,
     stop: F,
 }
 
-impl<T, F> PowerSet<T, F>
-    where T: Copy,
+impl<'a, T, F> PowerSet<'a, T, F>
+    where T: 'a + Copy,
           F: Fn(&[T]) -> bool
 {
     // not sure why this doesn't compile
     // expected type parameter, found fn item
     //
-    //  fn new(data: &[T]) -> PowerSet<T, F> {
+    //  fn new(data: &'a [T]) -> PowerSet<'a, T, F> {
     //      PowerSet::with_prune_condition(data, |_| false)
     //  }
 
-    fn with_prune_condition(data: &[T], stop: F) -> PowerSet<T, F> {
+    fn with_prune_condition(data: &'a [T], stop: F) -> PowerSet<'a, T, F> {
         let mut stack = Vec::new();
         if let Some((selected, remaining)) = data.split_last() {
-            stack.push((vec![*selected], remaining.to_vec()));
-            stack.push((vec![], remaining.to_vec()));
+            stack.push((vec![*selected], remaining));
+            stack.push((vec![], remaining));
         } else {
-            stack.push((vec![], vec![]));
+            // stack.push((vec![], &[T::zero(); 0][..]))
+            stack.push((vec![], data))
         }
         PowerSet {
             stack: stack,
@@ -55,8 +56,8 @@ impl<T, F> PowerSet<T, F>
     }
 }
 
-impl<T, F> Iterator for PowerSet<T, F>
-    where T: Copy,
+impl<'a, T, F> Iterator for PowerSet<'a, T, F>
+    where T: 'a + Copy,
           F: Fn(&[T]) -> bool
 {
     type Item = Vec<T>;
@@ -64,11 +65,11 @@ impl<T, F> Iterator for PowerSet<T, F>
     fn next(&mut self) -> Option<Vec<T>> {
         if let Some((selected, remaining)) = self.stack.pop() {
             if let Some((sel, rem)) = remaining.split_last() {
-                let mut r = selected.clone();
-                r.push(*sel);
                 if !(self.stop)(&selected) {
-                    self.stack.push((r, rem.to_vec()));
-                    self.stack.push((selected.clone(), rem.to_vec()));
+                    let mut r = selected.clone();
+                    r.push(*sel);
+                    self.stack.push((r, rem));
+                    self.stack.push((selected, rem));
                 }
                 self.next()
             } else {
