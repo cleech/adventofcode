26e4c1e715f7e3112d17f8542c8fc59b9c48da4b
Chris Leech <leech@pobox.com>
2016-01-07 10:46:00 -0800

    day05: rework without using pcre regex
    I was using pcre, because the standard rust regex crate doesn't support
    back-references (which make these really easy).  But it's not very hard
    to do these without regex (especially now that I know about .window()).

--- src/day05.rs
+++ src/day05.rs
@@ -1,49 +1,64 @@
-extern crate pcre;
-use self::pcre::Pcre;
+use std::str;
 
 const DATA: &'static str = include_str!("../data/input_5.txt");
 
 pub fn main() -> Vec<String> {
     let s1 = nice_count(DATA);
-    let s2 = new_nice_count(DATA);
+    let s2 = nicer_count(DATA);
     vec![s1.to_string(), s2.to_string()]
 }
 
+fn nice(s: &str) -> bool {
+    let has_three_vowels = |s: &str| s.chars().filter(|c| "aeiou".contains(*c)).count() >= 3;
+    let has_doubles = |s: &str| s.as_bytes().windows(2).any(|w| w[0] == w[1]);
+    let has_bad_pairs = |s: &str| {
+        s.as_bytes()
+         .windows(2)
+         .any(|w| ["ab", "cd", "pq", "xy"].contains(&(unsafe { str::from_utf8_unchecked(w) })))
+    };
+    has_three_vowels(s) && has_doubles(s) && !has_bad_pairs(s)
+}
+
 fn nice_count(input: &str) -> usize {
-    let mut vowels = Pcre::compile(r"[aeiou]").unwrap();
-    let mut has_double = Pcre::compile(r"(.)\1").unwrap();
-    let mut bad_pairs = Pcre::compile(r"(ab|cd|pq|xy)").unwrap();
-
     input.lines()
-         .filter(|line| vowels.matches(line).count() >= 3)
-         .filter(|line| has_double.exec(line).is_some())
-         .filter(|line| bad_pairs.exec(line).is_none())
+         .filter(|line| nice(line))
          .count()
 }
 
-#[test]
-fn test_nice_string() {
-    assert_eq!(nice_count("ugknbfddgicrmopn"), 1);
-    assert_eq!(nice_count("aaa"), 1);
-    assert_eq!(nice_count("jchzalrnumimnmhp"), 0);
-    assert_eq!(nice_count("haegwjzuvuyypxyu"), 0);
-    assert_eq!(nice_count("dvszwmarrgswjxmb"), 0);
+fn nicer(s: &str) -> bool {
+    let has_double_pairs = |s: &str| {
+        let bs = s.as_bytes();
+        (0..bs.len() - 3)
+            .any(|n| bs[n + 2..].windows(2).any(|w| w[0] == bs[n] && w[1] == bs[n + 1]))
+    };
+    let has_sandwich = |s: &str| s.as_bytes().windows(3).any(|w| w[0] == w[2]);
+    has_double_pairs(s) && has_sandwich(s)
 }
 
-fn new_nice_count(input: &str) -> usize {
-    let mut pairs = Pcre::compile(r"(..).*\1").unwrap();
-    let mut sandwich = Pcre::compile(r"(.).\1").unwrap();
-
+fn nicer_count(input: &str) -> usize {
     input.lines()
-         .filter(|line| pairs.exec(line).is_some())
-         .filter(|line| sandwich.exec(line).is_some())
+         .filter(|line| nicer(line))
          .count()
 }
 
-#[test]
-fn test_new_nice_string() {
-    assert_eq!(new_nice_count("qjhvhtzxzqqjkmpb"), 1);
-    assert_eq!(new_nice_count("xxyxx"), 1);
-    assert_eq!(new_nice_count("uurcxstgmygtbstg"), 0);
-    assert_eq!(new_nice_count("eodomkazucvgmuy"), 0);
+#[cfg(test)]
+mod test {
+    use super::{nice_count, nicer_count};
+
+    #[test]
+    fn test_nice_string() {
+        assert_eq!(nice_count("ugknbfddgicrmopn"), 1);
+        assert_eq!(nice_count("aaa"), 1);
+        assert_eq!(nice_count("jchzalrnumimnmhp"), 0);
+        assert_eq!(nice_count("haegwjzuvuyypxyu"), 0);
+        assert_eq!(nice_count("dvszwmarrgswjxmb"), 0);
+    }
+
+    #[test]
+    fn test_nicer_string() {
+        assert_eq!(nicer_count("qjhvhtzxzqqjkmpb"), 1);
+        assert_eq!(nicer_count("xxyxx"), 1);
+        assert_eq!(nicer_count("uurcxstgmygtbstg"), 0);
+        assert_eq!(nicer_count("eodomkazucvgmuy"), 0);
+    }
 }
