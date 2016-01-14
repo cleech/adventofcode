ef3b838808fdf0b5190a80a5ea25a129c6e3c24f
Chris Leech <leech@pobox.com>
2016-01-13 21:26:25 -0800

    day 24: performance improvement with better dynamic search limits
    Don't just limit the search space by the total weight sum, but also with
    the best known solution so far for both package count and quantum
    entanglement.
    
    I'm really happy with how combining the Cell type with a reference
    captured to a closure and updating the value from elsewhere in the
    iterator chain worked out.  I didn't think this would work without
    hacking into the generic PowerSet generator more, but I only had one
    lifetime annotation to fix up.

--- src/day24.rs
+++ src/day24.rs
@@ -1,31 +1,53 @@
 const DATA: &'static str = include_str!("../data/input_24.txt");
 
-pub fn main() -> Vec<String> {
-    let v = DATA.lines().map(|l| l.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-    let weight = v.iter().sum::<u64>() / 3;
-    let s1 = min_qe(&v, weight).to_string();
+use std::cmp;
+use std::{u64, usize};
+use std::cell::Cell;
 
-    let weight = v.iter().sum::<u64>() / 4;
-    let s2 = min_qe(&v, weight).to_string();
-    vec![s1, s2]
+pub fn main() -> Vec<String> {
+    if let Ok(mut v) = DATA.lines()
+                           .map(|l| l.parse::<u64>())
+                           .collect::<Result<Vec<u64>, _>>() {
+        v.sort_by(|a, b| b.cmp(a));
+
+        let weight = v.iter().sum::<u64>() / 3;
+        let s1 = min_qe(&v, weight).unwrap().to_string();
+
+        let weight = v.iter().sum::<u64>() / 4;
+        let s2 = min_qe(&v, weight).unwrap().to_string();
+
+        vec![s1, s2]
+    } else {
+        vec![]
+    }
 }
 
-fn min_qe(data: &[u64], weight: u64) -> u64 {
-    let ps = PowerSet::with_prune_condition(&data, move |ss| ss.iter().sum::<u64>() > weight);
-    let subsets = ps.filter(|ss| ss.iter().sum::<u64>() == weight).collect::<Vec<_>>();
-    let size = subsets.iter().map(|ss| ss.len()).min().unwrap();
+fn min_qe(data: &[u64], weight: u64) -> Option<u64> {
+    let best_size: Cell<usize> = Cell::new(usize::MAX);
+    let best_qe: Cell<u64> = Cell::new(u64::MAX);
+    let stop = |ss: &[u64]| {
+        (ss.iter().sum::<u64>() > weight) ||
+        (ss.iter().product::<u64>() > best_qe.get()) ||
+        (ss.len() > best_size.get())
+    };
+    let ps = PowerSet::with_prune_condition(&data, stop);
+    let subsets = ps.filter(|ss| ss.iter().sum::<u64>() == weight)
+                    .inspect(|ss| {
+                        best_size.set(cmp::min(best_size.get(), ss.len()));
+                        best_qe.set(cmp::min(best_qe.get(), ss.iter().product()));
+                    })
+                    .collect::<Vec<_>>();
     subsets.iter()
-           .filter(|ss| ss.len() == size)
+           .filter(|ss| ss.len() == best_size.get())
            .map(|ss| ss.iter().product::<u64>())
            .min()
-           .unwrap()
 }
 
 struct PowerSet<'a, T>
     where T: 'a + Copy
 {
     stack: Vec<(Vec<T>, &'a [T])>,
-    stop: Box<Fn(&[T]) -> bool>,
+    stop: Box<Fn(&[T]) -> bool + 'a>,
 }
 
 impl<'a, T> PowerSet<'a, T> where T: 'a + Copy
@@ -45,14 +67,13 @@ impl<'a, T> PowerSet<'a, T> where T: 'a + Copy
     }
 
     fn with_prune_condition<F>(data: &'a [T], stop: F) -> PowerSet<'a, T>
-        where F: 'static + Fn(&[T]) -> bool
+        where F: 'a + Fn(&[T]) -> bool
     {
         PowerSet { stop: box stop, ..PowerSet::new(data) }
     }
 }
 
-impl<'a, T> Iterator for PowerSet<'a, T>
-    where T: 'a + Copy,
+impl<'a, T> Iterator for PowerSet<'a, T> where T: 'a + Copy
 {
     type Item = Vec<T>;
 
