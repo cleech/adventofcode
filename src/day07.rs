bd41bf804c8c5d8b427f411929c302342d0a1f86
Chris Leech <leech@pobox.com>
2016-01-18 13:46:45 -0800

    day07: more cleanup + remove unwrap calls

--- src/day07.rs
+++ src/day07.rs
@@ -2,12 +2,9 @@ use std::str::FromStr;
 use std::collections::HashMap;
 
 #[derive(PartialEq, Eq, Hash, Clone)]
-struct Wire(pub String);
-
-#[derive(PartialEq, Eq, Hash, Clone)]
 enum Source {
     Pattern(u16),
-    Wire(Wire),
+    Wire(String),
 }
 
 impl FromStr for Source {
@@ -20,7 +17,7 @@ impl FromStr for Source {
                 s.parse::<u16>().map_err(|_| "parse error").map(Source::Pattern)
             }
             s if (|s: &str| s.chars().all(|c| c.is_alphabetic()))(s) => {
-                return Ok(Source::Wire(Wire(s.to_owned())))
+                Ok(Source::Wire(s.to_owned()))
             }
             _ => Err("unknown identifier format"),
         }
@@ -65,83 +62,63 @@ impl FromStr for Gate {
         match &s.split_whitespace().collect::<Vec<_>>()[..] { 
             [a, "->", o] => {
                 a.parse::<Source>()
-                 .and_then(|a| {
-                     o.parse::<Source>()
-                      .map(|o| Gate::Pass { src: a, out: o })
-                 })
+                 .and_then(|a| o.parse::<Source>().map(|o| Gate::Pass { src: a, out: o }))
             }
             ["NOT", a, "->", o] => {
                 a.parse::<Source>()
-                 .and_then(|a| {
-                     o.parse::<Source>()
-                      .map(|o| Gate::Not { src: a, out: o })
-                 })
+                 .and_then(|a| o.parse::<Source>().map(|o| Gate::Not { src: a, out: o }))
             }
             [a, "AND", b, "->", o] => {
-                a.parse::<Source>()
-                 .and_then(|a| {
-                     b.parse::<Source>()
-                      .and_then(|b| {
-                          o.parse::<Source>()
-                           .map(|o| {
-                               Gate::And {
-                                   a: a,
-                                   b: b,
-                                   out: o,
-                               }
-                           })
-                      })
-                 })
+                a.parse::<Source>().and_then(|a| {
+                    b.parse::<Source>().and_then(|b| {
+                        o.parse::<Source>().map(|o| {
+                            Gate::And {
+                                a: a,
+                                b: b,
+                                out: o,
+                            }
+                        })
+                    })
+                })
             }
             [a, "OR", b, "->", o] => {
-                a.parse::<Source>()
-                 .and_then(|a| {
-                     b.parse::<Source>()
-                      .and_then(|b| {
-                          o.parse::<Source>()
-                           .map(|o| {
-                               Gate::Or {
-                                   a: a,
-                                   b: b,
-                                   out: o,
-                               }
-                           })
-                      })
-                 })
+                a.parse::<Source>().and_then(|a| {
+                    b.parse::<Source>().and_then(|b| {
+                        o.parse::<Source>().map(|o| {
+                            Gate::Or {
+                                a: a,
+                                b: b,
+                                out: o,
+                            }
+                        })
+                    })
+                })
             }
             [a, "LSHIFT", n, "->", o] => {
-                a.parse::<Source>()
-                 .and_then(|a| {
-                     n.parse::<u16>()
-                      .map_err(|_| "parse error")
-                      .and_then(|n| {
-                          o.parse::<Source>()
-                           .map(|o| {
-                               Gate::LShift {
-                                   src: a,
-                                   shift: n,
-                                   out: o,
-                               }
-                           })
-                      })
-                 })
+                a.parse::<Source>().and_then(|a| {
+                    n.parse::<u16>().map_err(|_| "parse error").and_then(|n| {
+                        o.parse::<Source>().map(|o| {
+                            Gate::LShift {
+                                src: a,
+                                shift: n,
+                                out: o,
+                            }
+                        })
+                    })
+                })
             }
             [a, "RSHIFT", n, "->", o] => {
-                a.parse::<Source>()
-                 .and_then(|a| {
-                     n.parse::<u16>()
-                      .map_err(|_| "parse error")
-                      .and_then(|n| {
-                          o.parse::<Source>()
-                           .map(|o| {
-                               Gate::RShift {
-                                   src: a,
-                                   shift: n,
-                                   out: o,
-                               }
-                           })
-                      })
-                 })
+                a.parse::<Source>().and_then(|a| {
+                    n.parse::<u16>().map_err(|_| "parse error").and_then(|n| {
+                        o.parse::<Source>().map(|o| {
+                            Gate::RShift {
+                                src: a,
+                                shift: n,
+                                out: o,
+                            }
+                        })
+                    })
+                })
             }
             _ => Err("bad input"),
         }
@@ -190,21 +167,28 @@ struct Circuit {
     cache: HashMap<Source, u16>,
 }
 
-impl Circuit {
-    fn from_str(input: &str) -> Circuit {
+impl FromStr for Circuit {
+    type Err = &'static str;
+
+    fn from_str(input: &str) -> Result<Circuit, Self::Err> {
         let mut circuit = HashMap::new();
         let cache = HashMap::new();
 
         for line in input.lines() {
-            let gate = Gate::from_str(line).unwrap();
-            circuit.insert(gate.out(), gate);
+            if let Ok(gate) = Gate::from_str(line) {
+                circuit.insert(gate.out(), gate);
+            } else {
+                return Err("gate parse error");
+            }
         }
-        Circuit {
+        Ok(Circuit {
             gates: circuit,
             cache: cache,
-        }
+        })
     }
+}
 
+impl Circuit {
     fn eval(&mut self, wire: &Source) -> Option<u16> {
         match *wire {
             Source::Pattern(p) => Some(p),
@@ -212,9 +196,12 @@ impl Circuit {
                 if self.cache.contains_key(s) {
                     self.cache.get(s).cloned()
                 } else {
-                    let v = self.gates.get(s).cloned().and_then(|g| g.eval(self));
-                    self.cache.insert(s.clone(), v.unwrap());
-                    v
+                    if let Some(v) = self.gates.get(s).cloned().and_then(|g| g.eval(self)) {
+                        self.cache.insert(s.clone(), v);
+                        Some(v)
+                    } else {
+                        None
+                    }
                 }
             }
         }
@@ -226,20 +213,27 @@ impl Circuit {
     }
 }
 
-fn run_circuit(input: &str, target: &str) -> u16 {
-    let mut c = Circuit::from_str(input);
-    c.eval(&Source::Wire(Wire(target.to_owned()))).unwrap()
+fn run_circuit(input: &str, target: &str) -> Option<u16> {
+    if let Ok(mut c) = Circuit::from_str(input) {
+        c.eval(&Source::Wire(target.to_owned()))
+    } else {
+        None
+    }
 }
 
-fn part2(input: &str, target: &str, force: &str, v: u16) -> u16 {
-    let mut c = Circuit::from_str(input);
-    c.force(&Source::Wire(Wire(force.to_owned())), v);
-    c.eval(&Source::Wire(Wire(target.to_owned()))).unwrap()
+fn part2(input: &str, target: &str, force: &str, v: u16) -> Option<u16> {
+    if let Ok(mut c) = Circuit::from_str(input) {
+        c.force(&Source::Wire(force.to_owned()), v);
+        c.eval(&Source::Wire(target.to_owned()))
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use super::{Circuit, Source, Wire};
+    use std::str::FromStr;
+    use super::{Circuit, Source};
 
     #[test]
     fn test_circuit() {
@@ -252,22 +246,22 @@ mod test {
                   "NOT x -> h",
                   "NOT y -> i"]
                      .join("\n");
-        let mut c = Circuit::from_str(&cs);
-        assert_eq!(c.eval(&Source::Wire(Wire("d".to_owned()))), Some(72));
-        assert_eq!(c.eval(&Source::Wire(Wire("e".to_owned()))), Some(507));
-        assert_eq!(c.eval(&Source::Wire(Wire("f".to_owned()))), Some(492));
-        assert_eq!(c.eval(&Source::Wire(Wire("g".to_owned()))), Some(114));
-        assert_eq!(c.eval(&Source::Wire(Wire("h".to_owned()))), Some(65412));
-        assert_eq!(c.eval(&Source::Wire(Wire("i".to_owned()))), Some(65079));
-        assert_eq!(c.eval(&Source::Wire(Wire("x".to_owned()))), Some(123));
-        assert_eq!(c.eval(&Source::Wire(Wire("y".to_owned()))), Some(456));
+        let mut c = Circuit::from_str(&cs).unwrap();
+        assert_eq!(c.eval(&Source::Wire("d".to_owned())), Some(72));
+        assert_eq!(c.eval(&Source::Wire("e".to_owned())), Some(507));
+        assert_eq!(c.eval(&Source::Wire("f".to_owned())), Some(492));
+        assert_eq!(c.eval(&Source::Wire("g".to_owned())), Some(114));
+        assert_eq!(c.eval(&Source::Wire("h".to_owned())), Some(65412));
+        assert_eq!(c.eval(&Source::Wire("i".to_owned())), Some(65079));
+        assert_eq!(c.eval(&Source::Wire("x".to_owned())), Some(123));
+        assert_eq!(c.eval(&Source::Wire("y".to_owned())), Some(456));
     }
 }
 
 const DATA: &'static str = include_str!("../data/input_7.txt");
 
 pub fn main() -> Vec<String> {
-    let s1 = run_circuit(DATA, "a");
-    let s2 = part2(DATA, "a", "b", 956);
+    let s1 = run_circuit(DATA, "a").unwrap();
+    let s2 = part2(DATA, "a", "b", 956).unwrap();
     vec![s1.to_string(), s2.to_string()]
 }
