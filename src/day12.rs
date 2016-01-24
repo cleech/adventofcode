e256194e28d236621211b2f2d4c1ab990a534b37
Chris Leech <leech@pobox.com>
2016-01-23 22:35:56 -0800

    day 12: share code, split out filter condition

--- src/day12.rs
+++ src/day12.rs
@@ -12,36 +12,30 @@ pub fn main() -> Vec<String> {
 fn json_sum(s: &str) -> Result<i64, String> {
     serde_json::from_str(s)
         .map_err(|e| e.to_string())
-        .map(|ref v| json_to_num(v))
+        .map(|ref v| json_to_num(v, &|_| true))
 }
 
-fn json_to_num(data: &Value) -> i64 {
+fn json_sum_no_red(s: &str) -> Result<i64, String> {
+    serde_json::from_str(s)
+        .map_err(|e| e.to_string())
+        .map(|ref v| json_to_num(v, &|v| v.as_string().map_or(true, |s| s != "red")))
+}
+
+fn json_to_num<F>(data: &Value, filter: &F) -> i64
+    where F: Fn(&Value) -> bool
+{
     match *data {
         Value::I64(n) => n,
         Value::U64(n) => n as i64,
-        Value::Array(ref vec) => vec.iter().map(json_to_num).sum(),
-        Value::Object(ref bt) => bt.values().map(json_to_num).sum(),
-        _ => 0,
-    }
-}
-
-fn json_sum_no_red(input: &str) -> Result<i64, String> {
-    serde_json::from_str(input)
-        .map_err(|e| e.to_string())
-        .map(|ref v| json_to_num_no_red(v))
-}
-
-fn json_to_num_no_red(data: &Value) -> i64 {
-    match *data {
-        Value::Array(ref vec) => vec.iter().map(json_to_num_no_red).sum(),
+        Value::Array(ref vec) => vec.iter().map(|v| json_to_num(v, filter)).sum(),
         Value::Object(ref bt) => {
-            if bt.values().any(|v| v.as_string().map_or(false, |s| s == "red")) {
-                0
+            if bt.values().all(|v| filter(v)) {
+                bt.values().map(|v| json_to_num(v, filter)).sum()
             } else {
-                bt.values().map(json_to_num_no_red).sum()
+                0
             }
         }
-        ref jv => json_to_num(jv),
+        _ => 0,
     }
 }
 
@@ -50,7 +44,7 @@ mod test {
     use super::{json_sum, json_sum_no_red};
 
     #[test]
-    fn examples_part_1() {
+    fn examples_1() {
         assert_eq!(json_sum(r#"[1,2,3]"#), Ok(6));
         assert_eq!(json_sum(r#"{"a":2,"b":4}"#), Ok(6));
         assert_eq!(json_sum(r#"[[[3]]]"#), Ok(3));
@@ -62,7 +56,7 @@ mod test {
     }
 
     #[test]
-    fn examples_part_2() {
+    fn examples_2() {
         assert_eq!(json_sum_no_red(r#"[1,2,3]"#), Ok(6));
         assert_eq!(json_sum_no_red(r#"[1,{"c":"red","b":2},3]"#), Ok(4));
         assert_eq!(json_sum_no_red(r#"{"d":"red","e":[1,2,3,4],"f":5}"#), Ok(0));
